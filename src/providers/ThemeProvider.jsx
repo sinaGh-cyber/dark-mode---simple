0de71e350b0c3961a76e4dbb63ebc1b8aa6c8769
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-12-25 13:35:56 +0330

    ContextComponent partly ready

--- src/providers/ThemeProvider.jsx
+++ src/providers/ThemeProvider.jsx
@@ -1,7 +1,21 @@
-import React from "react";
-import { THEME_TYPE } from "../constants";
+// import React, { useContext } from 'react';
+import { createContext } from 'react/cjs/react.development';
+import { useState } from 'react/cjs/react.production.min';
+// import { THEME_TYPE } from '../constants';
+
+const ThemeContext = createContext(undefined);
 
 const ThemeProvider = ({ children }) => {
-  return <>{children}</>;
+  const [ThemeMode, setThemeMode] = useState({isDark: false});
+
+  const ToggleTheme = (oldTheme) => {
+    setThemeMode({...oldTheme, isDarkMode: !oldTheme.isDarkMode})
+  };
+
+  return (<>
+    <ThemeContext.Provider value={{ ThemeMode, ToggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  </>);
 };
 export default ThemeProvider;
